674de2fb14ed6c74cd6aea1838c77f4d292e6603
Mir Billadin <mir.billadin4@gmail.com>
2023-10-15 09:46:02 +0600

    search by company added

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,19 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'Products testing route'})
+    const products = await Product.find({})
+    res.status(200).json({products})
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const { featured } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
